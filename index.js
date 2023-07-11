4d5e81488c9dc514928717291491ea5e35ea21aa
jcarlossc <josecscosta@fac.pe.senac.br>
2023-07-10 23:35:24 -0300

    início

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('../src/views/index');
+});
+
+app.listen(3000, function() {
+	console.log('Porta: 3000');
+});
