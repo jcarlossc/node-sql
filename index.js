8e4ef7908796403590f415a9cf4dd58f070aaa7b
jcarlossc <josecscosta@fac.pe.senac.br>
2023-07-11 22:23:02 -0300

    bootstrap e partials

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static('./src/public'));
 
 app.get('/', function(req, res) {
     res.render('../src/views/index');
 });
 
+
+//---------------------------
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'nodedb1'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+connection.query('SELECT * FROM users;', function (error, results, fields) {
+    if (error) throw error;
+    for(i = 0; i < results.length; i++) {
+        console.log('Lista: ', results[i]);
+    }
+});
+
+
 app.listen(3000, function() {
 	console.log('Porta: 3000');
 });
+
